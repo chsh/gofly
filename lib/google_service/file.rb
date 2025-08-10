28c7994cd2b720f1ebbfff119fd4ca268637b428
CHIKURA Shinsaku <shinsaku@chikura.me>
2025-08-10 13:15:49 +0900

    Fix syntax error

--- lib/google_service/file.rb
+++ lib/google_service/file.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class GoogleService::File < GoogleService::Base
-  class_methods do
-    def me?(meta)
-      meta.kind == "drive#file"
+  concerning :TypeDetectable do
+    class_methods do
+      def me?(meta)
+        meta.kind == "drive#file"
+      end
     end
   end
 end
