4dca1153fd5969d2e132efa7170b0103b5bbc16c
CHIKURA Shinsaku <shinsaku@chikura.me>
2025-08-10 09:58:13 +0900

    go flying to go fly

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GoFlying
+module GoFly
   class Application < Rails::Application
     # For TZ
     config.time_zone = "Tokyo"
